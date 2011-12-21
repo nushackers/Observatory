b0da1f3ace973b7394d384b1d695193c9306a03c
Eli James <ejames@pandamian.com>
2011-12-21 21:06:04 +0800

    Giant commit (sorry).
    Changed feed styles.
    Removed todo.

--- observatory/urls.py
+++ observatory/urls.py
@@ -24,17 +24,17 @@ for model in (AuthorRequest,
 urlpatterns = patterns('',
     # Example:
     # (r'^observatory/', include('observatory.foo.urls')),
-    
+
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    
+
     # author requests
     (r'^author-request/approve/(\d+)/$', author_requests.approve),
     (r'^author-request/reject/(\d+)/$', author_requests.reject),
-    
+
     # blog posts
     (r'^posts/add/(\d+)/$', blogs.write_post),
     (r'^posts/create/(\d+)/$', blogs.create_post),
@@ -46,13 +46,13 @@ urlpatterns = patterns('',
     (r'^post/([^\.]*)/$', blogs.show_user_post),
     (r'^project/([^\.]*)/post/([^\.]*)\.rss$',
      SingleFeed(),  {'model': BlogPost}),
-    
+
     (r'^projects/([^\.]*)/posts/$', blogs.show_blog),
     (r'^projects/([^\.]*)/posts\.rss$', BlogPostsFeed()),
-    
+
     (r'^posts/$', blogs.posts),
     (r'^posts\.rss$', BlogPostsFeed()),
-    
+
     # users
     (r'^register-or-login/$', users.login_or_reg),
     (r'^register/$', users.register),
@@ -70,21 +70,21 @@ urlpatterns = patterns('',
 	(r'^user_activate/(\d+)/$', users.activate),
     (r'^forgot-password/$', users.forgot_password),
     (r'^forgot_password_success/$', users.forgot_password_success),
-    
+
     # commits
     (r'^projects/([^\.]*)/commit/([^\.]*)/$', commits.show),
     (r'^projects/([^\.]*)/commit/([^\.]*)\.rss$',
      SingleFeed(), {'model': Commit}),
-    
-    
+
+
     (r'^commits/(\d+)/$', commits.all_page),
     (r'^commits/$', commits.all),
     (r'^commits\.rss$', CommitsFeed()),
-    
+
     (r'^projects/([^\.]*)/commits/$', commits.show_repository),
     (r'^projects/([^\.]*)/commits/(\d+)/$', commits.repository_page),
     (r'^projects/([^\.]*)/commits\.rss$', CommitsFeed()),
-    
+
     # projects
     (r'^projects/add-user/$', projects.add_user),
     (r'^projects/remove-user/$', projects.remove_user),
@@ -95,21 +95,21 @@ urlpatterns = patterns('',
         projects.delete_screenshot),
     (r'^projects/([^\.]*)/modify/(\d+)/$', projects.modify),
     (r'^projects/([^\.]*)/modify/$', projects.modify),
-    
+
     (r'^projects/([^\.]*)/$', projects.show),
     (r'^projects/([^\.]*)\.rss', EventsFeed()),
-    
+
     (r'^projects/$', projects.list),
-    (r'^$', projects.list),
-	
+    (r'^$', feed.feed),
+
 	#tasks
-	(r'^todo/', include('todo.urls')),
-    
+	#(r'^todo/', include('todo.urls')), # Removed tasks from Treehouse
+
     # feed
     (r'^event/([^\.]*)/$', feed.event),
     (r'^feed/$', feed.feed),
     (r'^feed\.rss$', EventsFeed()),
-    
+
     # serve media (for now)
     (r'^site-media/(?P<path>.*)/$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
